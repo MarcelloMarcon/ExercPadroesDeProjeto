59d8307d8edfaf320452667bd0a35b90ebaa221e
Marcello Marcon <97744212+MarcelloMarcon@users.noreply.github.com>
2023-10-09 12:59:21 -0300

    Add files via upload

---
+++ src/Motor.java
@@ -0,0 +1,45 @@
+public class Motor {
+
+    private TipoCombustivel tipoMotor;
+    private int consumo; // em quilometros por unidade. Ex: Km/Lt
+    private int quilometragem;
+
+    public Motor(TipoCombustivel tipoMotor, int consumo) {
+        this.tipoMotor = tipoMotor;
+        this.consumo = consumo;
+    }
+
+    public int getConsumo(String ultimoAbastecimento,String modelo) {
+        if(modelo=="SUVFlex"){
+            if(ultimoAbastecimento=="GASOLINA"){
+                this.consumo=8;
+            }
+            else{
+                this.consumo=6;
+            }
+        }
+      
+        return this.consumo;
+    }
+
+    public TipoCombustivel getTipoMotor(){
+        return this.tipoMotor;
+    }
+
+    public int getQuilometragem(){
+        return this.quilometragem;
+    }
+
+    public int combustivelNecessario(int distancia) {
+        return distancia / consumo;
+    }
+
+    public void percorre(int distancia) {
+        quilometragem += distancia;
+    }
+
+    @Override
+    public String toString() {
+        return "Motor [consumo=" + consumo + ", quilometragem=" + quilometragem + ", tipoMotor=" + tipoMotor + "]";
+    }
+}
